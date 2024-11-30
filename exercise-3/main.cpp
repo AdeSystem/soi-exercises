c7a55bdc83aa458237d882ce01282994f797eefe
alewand2 <01187246@pw.edu.pl>
2024-11-30 10:09:02 +0100

    Created single consumer and producer loop.

--- exercise-3/main.cpp
+++ exercise-3/main.cpp
@@ -17,52 +17,82 @@ int makeProduct(int min, int max) {
 
 struct IntegerBuffer {
     int size;
-    int *body;
     int in = 0;
     int out = 0;
+    
+    int* body;
+    Semaphore* mutex;
+    Semaphore* empty;
+    Semaphore* full;
 
-    Semaphore mutex;
-    Semaphore empty;
-    Semaphore full;
+    static IntegerBuffer* create(int bufferSize) {
+        void* memory = mmap(nullptr, sizeof(IntegerBuffer), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+        
+        if (memory == MAP_FAILED) {
+            perror("mmap");
+            exit(1);
+        }
 
-    IntegerBuffer(int p_size) : size(p_size), body(new int[size]), mutex(Semaphore(1)), empty(Semaphore(size)), full(Semaphore(0)) {}
-    ~IntegerBuffer() { delete[] body; }
+        IntegerBuffer* buffer = reinterpret_cast<IntegerBuffer*>(memory);
 
-    void put(int product) {
-        empty.p();
-        mutex.p();
-        body[in] = product;
-        in = (in + 1) % size;
-        mutex.v();
-        full.v();
+        buffer -> size = bufferSize;
+        buffer -> in = 0;
+        buffer -> out = 0;
+
+        buffer -> body = reinterpret_cast<int*>(mmap(nullptr, sizeof(int) * bufferSize, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0));
+
+        if (buffer->body == MAP_FAILED) {
+            perror("mmap for body");
+            exit(1);
+        }
+
+        buffer -> mutex = new (mmap(nullptr, sizeof(Semaphore), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0)) Semaphore(1);
+        buffer -> empty = new (mmap(nullptr, sizeof(Semaphore), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0)) Semaphore(bufferSize);
+        buffer -> full = new (mmap(nullptr, sizeof(Semaphore), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0)) Semaphore(0);
+
+        return buffer;
     }
 
-    int get() {
-        full.p();
-        mutex.p();
-        int product = body[out];
-        out = (out + 1) % size;
-        mutex.v();
-        empty.v();
-        return product;
+    static void destroy(IntegerBuffer* buffer) {
+        munmap(buffer->body, sizeof(int) * buffer -> size);
+        buffer -> mutex -> ~Semaphore();
+        munmap(buffer -> mutex, sizeof(Semaphore));
+        buffer -> empty -> ~Semaphore();
+        munmap(buffer -> empty, sizeof(Semaphore));
+        buffer -> full -> ~Semaphore();
+        munmap(buffer -> full, sizeof(Semaphore));
+        munmap(buffer, sizeof(IntegerBuffer));
     }
 };
 
 void producer(IntegerBuffer *buffer, int min, int max) {
     while(true) {
             int product = makeProduct(min, max);
-            std::cout << "PRODUCER CREATED: " << product << std::endl;
-            buffer -> put(product);
-            std::cout << "PRODUCER PUT: " << product << std::endl;
+            buffer->empty->p();
+            std::cout << "PRODUCER entering critical section..." << std::endl;
+            buffer->mutex->p();
+            buffer->body[buffer->in] = product;
+            std::cout << "PRODUCER produced: " << product << " at index " << buffer->in << std::endl;
+            buffer->in = (buffer->in + 1) % buffer->size;
+            buffer->mutex->v();
+            buffer->full->v();
+            std::cout << "PRODUCER signals full, exiting critical section." << std::endl;
             usleep(1 * MUL);
         }
 }
 
 void consumer(IntegerBuffer* buffer) {
     while(true) {
-        std::cout << "CONSUMER GETS PRODUCT" << std::endl;
-        int product = buffer -> get();
-        std::cout << "CONSUMER GOT: " << product << std::endl;
+        std::cout << "CONSUMER waiting on full..." << std::endl;
+        buffer->full->p();
+        std::cout << "CONSUMER entering critical section..." << std::endl;
+        buffer->mutex->p();
+        int product = buffer->body[buffer->out];
+        std::cout << "CONSUMER consumed: " << product << " from index " << buffer->out << std::endl;
+        buffer->out = (buffer->out + 1) % buffer->size;
+        buffer->mutex->v();
+        buffer->empty->v();
+        std::cout << "CONSUMER signals empty, exiting critical section." << std::endl;
         usleep(1 * MUL);
     }
 }
@@ -75,11 +105,11 @@ void newConsumer(IntegerBuffer *buffer) {
     }
 }
 
-void newProducer(IntegerBuffer *buffer) {
+void newProducer(IntegerBuffer *buffer, int min, int max) {
     int created = fork();
     if (created == 0) {
         std::cout << "NEW PRODUCER PID: " << getpid() << std::endl;
-        producer(buffer, 1, 100);
+        producer(buffer, min, max);
     }
 }
 
@@ -99,7 +129,14 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    while (true) {}
+    IntegerBuffer* buffer = IntegerBuffer::create(bufferSize);
+
+    newProducer(buffer, 1, 100);
+    newConsumer(buffer);
+    
+    while(true) {}
+
+    IntegerBuffer::destroy(buffer);
 
     return 0;
 }
